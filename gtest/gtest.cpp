0a2745e7a43cba9e3f4fe17df297a4dc84f05c87
kuro <kuro@localhost.localdomain>
2020-05-20 18:56:57 +0800

    gtest

---
+++ gtest/gtest.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <gtest/gtest.h>
+#include <assert.h>
+
+using namespace std;
+
+double square_root(const double x)
+{
+    if (x < 0) return -1;
+    double sr = sqrt(x);
+    return sr;
+}
+
+TEST(square_root_test, positive_nums)
+{
+    EXPECT_EQ(2.0, square_root(4.0));
+    // EXPECT_EQ(1.73205, square_root(3.0));
+}
+
+TEST(square_root_test, zero_negetive_nums)
+{
+    EXPECT_EQ(0.0, square_root(0.0));
+    ASSERT_EQ(-1, square_root(-2.0));
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    string A = "fasdfaf";
+    int tmp;
+    tmp = RUN_ALL_TESTS();
+    assert(1.0 == square_root(4.0));
+    return 0;
+}
